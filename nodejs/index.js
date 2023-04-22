44b179ba3f9abb4271c764c21dddac23663f6a8c
root <root@pc.localdomain>
2023-04-22 13:11:33 -0300

    Corrige codigo Para criar banco e puxar direto do banco

--- nodejs/index.js
+++ nodejs/index.js
@@ -10,40 +10,47 @@ const config = {
 };
 
 const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+const sql = `SELECT * from People;`
 
-const sql = `SELECT * from Persons;`
+const createTableSql = `CREATE TABLE IF NOT EXISTS People (id INT AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY(id));`
 
-connection.query(sql, (resp, results) => {
-    if(results) {
+const createTableSqlConn = mysql.createConnection(config)
+createTableSqlConn.query(createTableSql)
+createTableSqlConn.end()
+
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config)
+
+    connection.query(sql, (resp, results) => {
         let tableHtml = ''
 
-        results.forEach(r => {
-            tableHtml += `
-                <tr>
-                    <td>${r.id}</td>
-                    <td>${r.name}</td>
-                </tr>
-            `
-        })
-
-        app.get('/', (req,res) => {
+        if(!resp && results) {
+            results.forEach(r => {
+                tableHtml += `
+                    <tr>
+                        <td>${r.id}</td>
+                        <td>${r.name}</td>
+                    </tr>
+                `
+            })
+    
             res.send(`<h1>Full Cycle Rocks!</h1>
-            <table>
-                <tr>
-                <th>ID</th>
-                <th>Name</th>
-                </tr>
-                ${tableHtml}
-            </table>`)
-        })
-        
-        return app.listen(port, ()=> {
-            console.log('Rodando na porta ' + port)
-        })  
-    }
-
-    console.error('People not found')
+                <table>
+                    <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    </tr>
+                    ${tableHtml.length ? tableHtml : '<th>-</th><th>Sem resultados</th>'}
+                </table>`)
+            return 
+        }
+    
+        return res.send(`<h1>Full Cycle Rocks!</h1><p>Erro ao conectar no banco de dados</p>`)
+    })
+    
+    connection.end()
 })
 
-connection.end()
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})  
